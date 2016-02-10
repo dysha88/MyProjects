a02ee5c855e8a221c0625638537c6dbed5450ba5
DYSHA <dysha88@meta.ua>
2016-02-10 23:07:09 +0200

    commit

--- Canvas/public_html/js/HomeWorks/test.js
+++ Canvas/public_html/js/HomeWorks/test.js
@@ -7,6 +7,8 @@ function Circle(x, y, r){
     this.x = x;
     this.y = y;
     this.r = r;
+    this.width = 0;
+    this.heigth = 0;
     this.arrColor = ['green', 'blue', 'red', 'black', 'yellow'];
     this.color = me.arrColor[parseInt(Math.random()*6)];
     this.draw = function(ctx){
@@ -22,39 +24,57 @@ function Circle(x, y, r){
         me.x = x1;
         me.y = y1;
     };
-    this.x1 = Math.random();
-    this.y1 = Math.random();
-    this.speed = parseInt(Math.random()*10);
-    var time = 0;
-    this.redraw = function(){
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        me.setPosition(me.x + time*me.x1*me.speed, me.y + time*me.y1*me.speed);
-        me.draw(context);
-//        window.requestAnimationFrame(me.redraw);
-        
+    this.speedX = parseInt(Math.random()*10) + 1;
+    this.speedY = parseInt(Math.random()*10) + 1;
+    this.setArea = function (width, height){
+        me.width = width;
+        me.height = height;
     };
-    this.update = function(){
+    var time = 0;
+    this.update = function(ctx){
+        
+        if((me.x + me.speedX > me.width) || (me.x + me.speedX< 0)){
+            me.speedX = -me.speedX;
+            me.setPosition(me.x + me.speedX, me.y + me.speedY);
+            me.draw(ctx);
+       
+        };
+        if((me.y + me.speedY > me.height) || (me.y + me.speedY < 0)){
+            me.speedY = -me.speedY;
+            me.setPosition(me.x + me.speedX, me.y + me.speedY);
+            me.draw(ctx);
+            
+        };
+        me.setPosition(me.x + me.speedX, me.y + me.speedY);
+        me.draw(ctx);
+    };
+    this.redraw = function(){
         time++;
         console.log(time);
-        console.log(me.speed);
-        me.redraw();
-        window.requestAnimationFrame(me.update);
+        me.update(context);
     };
 };
 
 var arr = [];
 var i;
-for(i = 0; i < 3; i++){
-    var circle = new Circle(150+i*70, 150+i*20, 30);
+for(i = 0; i < 30; i++){
+    var circle = new Circle(150+i*7, 150+i*2, 20);
     arr.push(circle);
-    arr[i].draw(context);
+    arr[i].setArea(canvas.width, canvas.height);
+    
 };
 console.log(arr);
-/*for(i = 0; i < 3; i++){
-    arr[i].redraw();
-    arr[i].update();
-};*/
-var circle = new Circle(150, 150, 30);
-circle.update();
+
+function move(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for(i = 0; i < arr.length; i++){
+        arr[i].redraw();
+
+        
+    };
+    window.requestAnimationFrame(move);
+};
+move();
+
 
 
