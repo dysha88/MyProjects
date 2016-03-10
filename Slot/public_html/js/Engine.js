f3537673f19a7ba00e18abd43f229fd13dba321a
andrei.nekhin <andrei.nekhin@D-CZC4240JXY.office.necorp.dom>
2016-03-10 17:41:30 +0200

    commit_29
    changes in Slot project

--- Slot/public_html/js/Engine.js
+++ Slot/public_html/js/Engine.js
@@ -1,4 +1,8 @@
 var WIDTH = 900;
 var HEIGHT = 500;
 
+var render = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {background : 0x1099bb});
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
 
--- Slot/public_html/js/Reels.js
+++ Slot/public_html/js/Reels.js
@@ -1,6 +1,15 @@
-function Reels (reel){
+function Reels (x, y, reelStrip){
     var me = this;
-    this.reel = reel;
+    this.x = x;
+    this.y = y;
+    this.reelStrip = reelStrip;
+    //this.imgName = "images/" + reelStrip[i] + ".png";
+    this.item = new PIXI.fromImage();
+    this.item1 = new PIXI.Graphics();
+    me.item1.drawImage()
+
+
+
     
 };
 
